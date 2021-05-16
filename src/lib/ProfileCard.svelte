e0cc925f342241e965f5a7c79edcd70581cc5f4c
Jordan Mann <jordaloo@gmail.com>
2021-05-16 01:27:33 -0700

    Extremely slick profiles

--- src/lib/ProfileCard.svelte
+++ src/lib/ProfileCard.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
     import { getProfile } from "$lib/firebase";
+    import { createEventDispatcher } from "svelte";
+    const dispatch = createEventDispatcher();
+
     export let uid: string;
+    export let expanded: boolean;
     const profile = getProfile(uid);
 
     const emojify = () => {
@@ -22,7 +26,7 @@
         let emoji = "";
         if ($profile.gender === "Male") emoji += unicode.man;
         else if ($profile.gender === "Female") emoji += unicode.woman;
-        else emoji += person;
+        else emoji += unicode.person;
         if ($profile["White"] !== "") emoji += unicode.light;
         else if ($profile["White"] !== "") emoji += unicode.light;
         else if ($profile["Black or African American"] !== "")
@@ -43,57 +47,82 @@
 </script>
 
 {#if $profile}
-    <div tabindex="0" style="--favorite-color: {$profile.favoriteColor}">
+    <div
+        class="wrapper"
+        on:keydown={(e) => {
+            if (e.key === "Enter") {
+                e.preventDefault();
+                e.currentTarget.click();
+            }
+        }}
+        on:click={(e) => {
+            if (!expanded) e.currentTarget.scrollIntoView({ block: "center" });
+            dispatch("expand");
+        }}
+        class:expanded
+        tabindex="0"
+        style="--favorite-color: {$profile.favoriteColor}"
+    >
         <img
             loading="lazy"
             width="50"
             src={$profile.picture}
             alt="DALI member's profile."
         />
-        <table>
-            <th>{$profile.name} <small>{$profile.year}</small></th>
-            <tr>
+        <div class="data">
+            <b>{$profile.name} <small>{$profile.year}</small></b>
+            <span>
                 <span class="emoji">
                     {emojify()}
                 </span>
-                {$profile.role}</tr
+                {$profile.role}</span
             >
-        </table>
+            <span
+                >🎓 {$profile.major}
+                {#if $profile.modification !== ""}+ {$profile.modification}{/if}
+                {#if $profile.minor !== ""}<br /><small
+                        ><i>minoring in {$profile.minor}</i></small
+                    >{/if}
+            </span>
+        </div>
     </div>
 {/if}
 
 <style>
-    div {
+    div.wrapper {
         border: 0.1rem solid var(--black);
         box-sizing: border-box;
         margin: 0.5rem;
         position: relative;
-        min-width: 90%;
+        width: 80%;
+        margin: auto;
         display: grid;
-        grid-template-rows: 8rem 8rem;
+        grid-template-rows: 8rem 12rem;
         grid-template-columns: 8rem auto;
         background-color: transparent;
-        transition: background-color 0.2s, transform 0.2s, height 0.3s;
+        transition: background-color 0.2s, transform 0.2s, height 0.3s,
+            box-shadow 0.2s;
         cursor: pointer;
         height: 8rem;
         overflow-y: hidden;
     }
-    div.expanded {
-        height: 16rem;
+    div.wrapper.expanded {
+        height: 20rem;
     }
 
-    div:hover,
-    div:focus {
+    div.wrapper:hover,
+    div.wrapper:focus {
         background-color: var(--transp);
         transform: scale(1.01);
+        box-shadow: 0 0.2rem 0.5rem rgba(0, 0, 0, 0.5);
     }
 
-    div:hover > img,
-    div:focus > img {
+    div.wrapper:hover > img,
+    div.wrapper:focus > img {
         transform: scale(1.02);
     }
 
-    img {
+    div.wrapper > img {
         grid-column: 1;
         height: 80%;
         width: 80%;
@@ -107,16 +136,12 @@
         border-bottom-color: var(--favorite-color);
         transition: transform 0.2s;
     }
-    table {
-        grid-column: 2;
-        padding: 0.5rem;
+    div.data {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        padding: 0.5rem 0;
         box-sizing: border-box;
-    }
-    th {
-        height: max-content;
-        font-size: 130%;
-        font-weight: normal;
-        text-align: left;
     }
     span.emoji {
         font-size: 200%;
--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -2,20 +2,19 @@
     import { getAllProfilesID } from "$lib/firebase";
     const profiles = getAllProfilesID();
     import ProfileCard from "$lib/ProfileCard.svelte";
+    let selected: number | null = null;
 </script>
 
 {#if Object.keys($profiles).length === 0}
     <div class="splash">Loading profiles...</div>
 {:else}
     <div>
-        {#each Object.values($profiles) as uid}
-            <ProfileCard {uid} />
+        {#each Object.values($profiles) as uid, i}
+            <ProfileCard
+                on:expand={() => (selected = selected === i ? null : i)}
+                {uid}
+                expanded={i === selected}
+            />
         {/each}
     </div>
 {/if}
-
-<style>
-    div {
-        min-width: 90%;
-    }
-</style>
